31f89a227ad7e75187fb54bdcde1a91d702d836d
FavourPeters <favourpeters1@gmail.com>
2023-03-25 08:24:54 +0100

    more updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 // UI Elements
-let P_info = document.querySelector('#p-info'),
-    M_plan = document.querySelector('#step2-mo'),
+let M_plan = document.querySelector('#step2-mo'),
     Y_plan = document.querySelector('#step2-yr'),
     M_addon = document.querySelector('#step3-mo'),
     Y_addon = document.querySelector('#step3-yr'),
@@ -13,17 +12,21 @@ let P_info = document.querySelector('#p-info'),
     Toggle = document.querySelector('#check'),
     toggleY = document.querySelector('#check1'),
     confirm = document.querySelector('.confirm'),
+    Back = document.querySelector('.back'),
+    backWards = document.querySelector('.backwards'),
     all = document.querySelectorAll('#chall');
 
 
 // Events
 Next.addEventListener('click', function(e){
+     P_info = document.querySelector('#p-info');
      P_info.className += ' hidden';
-     Next.className += ' hidden';
+     e.target.parentElement.className += ' hidden';
 
      M_plan.classList.remove('hidden');
-     nextStep.classList.remove('hidden');
-    e.preventDefault();
+     nextStep.parentElement.classList.remove('hidden');
+
+    e.preventDefault;
 });
 
 Toggle.addEventListener('click', function(e){
@@ -44,7 +47,7 @@ function Nexts(e){
     e.target.parentElement.parentElement.nextElementSibling.children[0].classList.remove('hidden');
     e.target.parentElement.parentElement.nextElementSibling.children[2].classList.remove('hidden');
 
-    e.preventdefault()
+    e.preventdefault
 }
 
 La.addEventListener('click', Nextes)
@@ -52,9 +55,34 @@ function Nextes(e){
     e.target.parentElement.parentElement.className += ' hidden';
     e.target.parentElement.parentElement.nextElementSibling.children[0].classList.remove('hidden');
     e.target.parentElement.parentElement.nextElementSibling.children[2].classList.remove('hidden');
-
-    e.preventdefault()
+    
+    e.preventdefault
 }
 
+Back.addEventListener('click', goBack);
+backWards.addEventListener('click', previous);
+// Go back function
+function goBack(e){
+   let main = document.querySelector('#p-info');
+   const Next = document.querySelector('.next');
+   let back = document.querySelector('.back');
 
+   back.parentElement.parentElement.children[0].className += ' hidden';
+   back.parentElement.className += ' hidden'
+   main.classList.remove('hidden');
+   Next.parentElement.classList.remove('hidden');
+   
+   e.preventDefault
+}
 
+// backWards function for step 3
+function previous(e){
+    let last = document.querySelector('#step2-mo');
+
+    backWards.parentElement.parentElement.children[0].className += ' hidden';
+    backWards.parentElement.parentElement.children[2].className += ' hidden';
+
+    last.parentElement.classList.remove('hidden');
+
+    e.preventDefault
+}
